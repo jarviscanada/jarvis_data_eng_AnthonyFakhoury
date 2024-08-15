04d6e048e20a375774aaf2948de8c6e305564da6
Anthony Fakhoury <anthonyfakhoury@hotmail.com>
2024-08-15 20:13:49 +0000

    Had to update Java version to implement HTTP&JSON

--- core_java/jdbc/src/main/java/ca/jrvs/apps/API/RestApiCall.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/API/RestApiCall.java
@@ -1,1 +1,36 @@
+package ca.jrvs.apps.API;
 
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.net.URI;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public class RestApiCall {
+  static String apiKey = "baa2240413msh2e60e8817c8fcaap115d3djsn4db12cb6f9f6";
+  static String symbol = "MSFT";
+
+  public static void main(String[] args) {
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+        .header("X-RapidAPI-Key", apiKey)
+        .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+        .method("GET", HttpRequest.BodyPublishers.noBody())
+        .build();
+    try {
+      HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+      System.out.println(response.body());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (JsonMappingException e) {
+      e.printStackTrace();
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
